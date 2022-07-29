76991821945a056418b5edbd34d4ab3935819392
Gagan163264 <gagan.surathkal@gmail.com>
2022-07-29 20:52:02 +0530

    z3

--- err.py
+++ err.py
@@ -1,42 +1,90 @@
 import sys
+from z3 import *
 
-#epsilon = sys.float_info.epsilon
-epsilon = 0.1
+epsilon = RealVal(sys.float_info.epsilon)
+dela = RealVal(simplify(epsilon/2))
+delr = RealVal(0)
+#delr=RealVal(0.000049948692321777343750000000)
+#dela=RealVal(0.000000119209289550781250000000)
+set_option(precision=150,rational_to_decimal=True)
+
+def z3max(v1,v2):
+    if(str(simplify(v1-v2))[0]=='-'):
+        return [v2, True]
+    else:
+        return [v1, False]
+
+def error(val):
+    return z3max(simplify(delr*val),dela)[0]
+
+def get_max(lst):
+    max = None
+    for item in lst:
+        if max is None or z3max(max,item)[1]:
+            max = item
+    return item
+
+def get_min(lst):
+    min = None
+    for item in lst:
+        if min is None or not z3max(min,item)[1]:
+            min = item
+    return item
 
 class BoundedFloat():
-    def __init__(self,val, ulmt = None, llmt = None):  #fix bound generation
-        self.val=val
+    def __init__(self,val = None,fval = None,ulmt = None, llmt = None):  #Redefine ctr
+        if val == None:
+            self.real = None
+            self.flt = None
+            self.err = None
+            self.bounds = [0,0]
+            return
+
+        self.real = RealVal(val)
         if isinstance(val, str):
-            val = ord(val)
-        if isinstance(val, int):
-            if llmt is None:
-                self.val_l = val
+            if '.' in val:
+                val = float(val)
             else:
-                self.val_l = llmt
-            if ulmt is None:
-                self.val_u = val
+                val = int(val)
+
+        self.flt=val
+        self.err = error(val)  #fix function
+        self.bounds = [0,0]
+
+        if llmt is None:
+            if isinstance(val, float):
+                self.bounds[0] = simplify(self.real-self.err)
+            elif isinstance(val, int):
+                self.bounds[0] = RealVal(val)
+        else:
+            if isinstance(llmt, RealVal):
+                self.bounds[0] = llmt
             else:
-                self.val_u = ulmt
-        elif isinstance(val, float):
-            if llmt is None:
-                self.val_l = self.val-epsilon
+                self.bounds[0] = RealVal(llmt)
+        if ulmt is None:
+            if isinstance(val, float):
+                self.bounds[1] = simplify(self.real+self.err)
+            elif isinstance(val, int):
+                self.bounds[1] = RealVal(val)
+        else:
+            if isinstance(ulmt, RealVal):
+                self.bounds[1] = ulmt
             else:
-                self.val_l = llmt
-            if ulmt is None:
-                self.val_u = self.val+epsilon
-            else:
-                self.val_u = ulmt
+                self.bounds[1] = RealVal(ulmt)
+
 
     def __str__(self):
-        return "{0} [{1}, {2}]".format(self.val, self.val_l, self.val_u)
+        return "{0}, {1} {2} {3}".format(self.flt, self.real, self.bounds,self.err)
 
     def __add__(self, other):
         if not isinstance(other, BoundedFloat):
             other = BoundedFloat(other)
-        nval = self.val + other.val
-        nu = self.val_u + other.val_u
-        nl = self.val_l + other.val_l
-        return BoundedFloat(nval, nu, nl)
+        new = BoundedFloat()
+        new.real = simplify(self.real + other.real)
+        new.flt = self.flt + other.flt
+        new.bounds = [simplify(self.bounds[0] + other.bounds[0]), simplify(self.bounds[1] + other.bounds[1])]
+        new.err = simplify(self.err+other.err)
+        return new
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -44,10 +92,12 @@ class BoundedFloat():
     def __sub__(self, other):
         if not isinstance(other, BoundedFloat):
             other = BoundedFloat(other)
-        nval = self.val - other.val
-        nu = self.val_l - other.val_u
-        nl = self.val_u - other.val_l
-        return BoundedFloat(nval, nu, nl)
+        new = BoundedFloat()
+        new.real = simplify(self.real - other.real)
+        new.flt = self.flt - other.flt
+        new.bounds = [simplify(self.bounds[0] - other.bounds[0]), simplify(self.bounds[1] - other.bounds[1])]
+        new.err = simplify(self.err-other.err)
+        return new
 
     def __rsub__(self, other):
         return self.__sub__(other)
@@ -55,13 +105,30 @@ class BoundedFloat():
     def __mul__(self, other):
         if not isinstance(other, BoundedFloat):
             other = BoundedFloat(other)
-        nval = self.val * other.val
-        nu = max(self.val_l*other.val_l,self.val_u*other.val_l,self.val_l*other.val_u,self.val_u*other.val_u)
-        nl = min(self.val_l*other.val_l,self.val_u*other.val_l,self.val_l*other.val_u,self.val_u*other.val_u)
-        return BoundedFloat(nval, nu, nl)
+        new = BoundedFloat()
+        new.real = simplify(self.real*other.real)
+        new.flt = self.flt * other.flt
+        set = [simplify(self.bounds[0]*other.bounds[0]),simplify(self.bounds[1]*other.bounds[0]),simplify(self.bounds[0]*other.bounds[1]),simplify(self.bounds[1]*other.bounds[1])]
+        new.bounds = [get_min(set), get_max(set)]
+        new.err = simplify(self.err*other.err)
+        return new
 
     def __rmul__(self, other):
         return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if not isinstance(other, BoundedFloat):
+            other = BoundedFloat(other)
+        new = BoundedFloat()
+        new.real = simplify(self.real/other.real)
+        new.flt = self.flt / other.flt
+        set = [simplify(self.bounds[0]/other.bounds[0]),simplify(self.bounds[1]/other.bounds[0]),simplify(self.bounds[0]/other.bounds[1]),simplify(self.bounds[1]/other.bounds[1])]
+        new.bounds = [get_min(set), get_max(set)]
+        new.err = simplify(self.err/other.err)
+        return new
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
 
     def __eq__(self, other):                                  #complete (raise issue)
         if not isinstance(other, BoundedFloat):
@@ -83,17 +150,14 @@ class BoundedFloat():
         pass
 
     def __int__(self):
-        nval = int(self.val)
-        nu = int(self.val_u)
-        nl = int(self.val_l)
+        nval = int(self.flt)
         return nval  #fix
 
     def __float__(self):
         return self
 
 
-
 if __name__ == "__main__":
-    s1= BoundedFloat(5.4, 5.3, 5.5)
-    s2 = BoundedFloat(4.5, 4.4, 4.6)
-    print(s1-s2)
+    s1= BoundedFloat(22.3)
+    s2 = BoundedFloat(7.2)
+    print(s1+s2*3.4)
