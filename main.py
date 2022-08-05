6fb29917da374e1cf1bebc1e56068e73e5bedc2d
Gagan163264 <gagan.surathkal@gmail.com>
2022-08-05 18:11:04 +0530

    cleanup

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         filename  = sys.argv[1]
     else:
-        filename = 'test.c'
+        filename = 'test/sine.c'
     parser = argparse.ArgumentParser()
     parser.add_argument("cmd", help="Command list: 'show' to display global variables and defined functions, 'run' to profile function given by 'file'(default file:test.c, default function:main) 'q'/'exit' for quit")
     parser.add_argument("file", help="Input file, not much to describe(default = test.c)", metavar = '<filename>', nargs = '?', default = 'test.c')
     parser.add_argument("-f","--fname", help = "Name of function to be analyzed(default = main)", default = 'main')
-    parser.add_argument("--args", help = "Arguments for input function, if necessary", metavar = '<arg1>,<arg2>...', nargs = '+', default = None)
+#    parser.add_argument("--args", help = "Arguments for input function, if necessary", metavar = '<arg1>,<arg2>...', nargs = '+', default = None)
     parser.add_argument("-p","--precision", help="Float precision setting for Z3, number of decimal places in the real number", metavar = 200, type = int, default = 200)
     parser.add_argument("-li", help = "Number of loop iterations before break", metavar = 200, type = int, default = 200 )
     parser.add_argument("-l", help="Loop debug enable", action='store_true')
